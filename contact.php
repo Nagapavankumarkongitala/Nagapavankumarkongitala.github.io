c8c3c850e348158a30085ffb8b41f4bc721013c7
KONGITALA NAGA PAVAN KUMAR <kumar.knp.82@gmail.com>
2024-12-16 18:02:09 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,19 +1,21 @@
 <?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$from = 'Parax Contact Form'; 
-		$to = 'kumar.knp.82@gmail.com'; 
-		$subject = $_POST['subject']; 
-		
-		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
-	
+    if (isset($_POST["submit"])) {
+        $name = strip_tags(trim($_POST['name']));
+        $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST['message']);
+        $subject = strip_tags(trim($_POST['subject']));
+        $to = 'kumar.knp.82@gmail.com'; 
+        $from = 'From: Parax Contact Form <' . $email . '>';
 
-	mail($to, $subject, $body, $from) or die("Error!");
+        $body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
 
-	header("location: thank-you.html");
-	
-	}
-	
+        if (mail($to, $subject, $body, $from)) {
+            header("Location: thank-you.html");
+            exit;
+        } else {
+            echo "Error! Please try again.";
+        }
+    }
+?>
+
 ?>
