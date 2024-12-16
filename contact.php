06e4ff3ea150b17406ec7297be8122b8a354ac7e
KONGITALA NAGA PAVAN KUMAR <kumar.knp.82@gmail.com>
2024-12-16 18:08:32 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,21 +1,25 @@
 <?php
-    if (isset($_POST["submit"])) {
-        $name = strip_tags(trim($_POST['name']));
-        $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST['message']);
-        $subject = strip_tags(trim($_POST['subject']));
-        $to = 'kumar.knp.82@gmail.com'; 
-        $from = 'From: Parax Contact Form <' . $email . '>';
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST["submit"])) {
+            var_dump($_POST); // Debugging line
+            
+            $name = strip_tags(trim($_POST['name']));
+            $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+            $message = trim($_POST['message']);
+            $subject = strip_tags(trim($_POST['subject']));
+            $to = 'kumar.knp.82@gmail.com'; 
+            $headers = "From: Parax Contact Form <$email>\r\nReply-To: $email";
 
-        $body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
+            $body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
 
-        if (mail($to, $subject, $body, $from)) {
-            header("Location: thank-you.html");
-            exit;
-        } else {
-            echo "Error! Please try again.";
+            if (mail($to, $subject, $body, $headers)) {
+                header("Location: thank-you.html");
+                exit;
+            } else {
+                echo "Error! Please try again.";
+            }
         }
+    } else {
+        echo "405 Method Not Allowed";
     }
-?>
-
 ?>
